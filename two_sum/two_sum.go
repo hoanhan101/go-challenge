d0de84a4f49453b6428b603e6f7b7f7888ce51ff
Hoanh An <hoanhan@bennington.edu>
2018-05-30 10:41:57 -0400

    rephrase documentation

--- two_sum/two_sum.go
+++ two_sum/two_sum.go
@@ -28,9 +28,7 @@ func twoSumI(nums []int, target int) []int {
 // twoSumII takes a sorted array of integers and a target integer number.
 // Similar to twoSumII, the goal is to return an array of indices of two numbers that add up to the
 // target. The only difference here is that input array is already sorted in ascending order.
-// We could still apply the hash table approach but it costs O(n) extra space, plus it does not
-// make use of the fact that the input is already sorted. Therefore, we are using pointers
-// approach: i start at first index, j start at the last.
+// This function uses pointers approach: i start at first index, j start at the last.
 // If the sum is larger than the target, decrement j to make it smaller.
 // If the sum is smaller than the target, increase i to make it bigger.
 // If the sum is equal to the target, we find the answer.
