660ca68f217376246fa9987035e51e0586233196
Hoanh An <hoanhan@bennington.edu>
2018-05-30 10:25:44 -0400

    add two sum ii problem

--- two_sum/two_sum.go
+++ two_sum/two_sum.go
@@ -5,9 +5,11 @@
 
 package two_sum
 
-// twoSumI takes an array of integers and a target integer number.
+// twoSumI takes an unsorted array of integers and a target integer number.
 // It returns an array of indices of two numbers that add up to the target.
-// Using a hash table method, it takes O(n) run time, O(n) space.
+// This function uses a hash map approach that maps a value to its index so we can lookup quickly.
+// Time complexity: O(n)
+// Space complexity: O(n)
 func twoSumI(nums []int, target int) []int {
 	tempDict := make(map[int]int)
 
@@ -19,6 +21,37 @@ func twoSumI(nums []int, target int) []int {
 		}
 	}
 
-	// Return [0, 0] if cannot find any
+	// Return [0, 0] if cannot find any.
+	return []int{0, 0}
+}
+
+// twoSumII takes a sorted array of integers and a target integer number.
+// Similar to twoSumII, the goal is to return an array of indices of two numbers that add up to the
+// target. The only difference here is that input array is already sorted in ascending order.
+// We could still apply the hash table approach but it costs O(n) extra space, plus it does not
+// make use of the fact that the input is already sorted. Therefore, we are using pointers
+// approach: i start at first index, j start at the last.
+// If the sum is larger than the target, decrement j to make it smaller.
+// If the sum is smaller than the target, increase i to make it bigger.
+// If the sum is equal to the target, we find the answer.
+// Time complexity: O(n)
+// Space complexity: O(1)
+func twoSumII(nums []int, target int) []int {
+	sum, i, j := 0, 0, len(nums)-1
+
+	for i < j {
+		sum = nums[i] + nums[j]
+		if sum == target {
+			return []int{i, j}
+		}
+
+		if sum < target {
+			i++
+		} else {
+			j--
+		}
+	}
+
+	// Return [0, 0] if cannot find any.
 	return []int{0, 0}
 }
--- two_sum/two_sum_test.go
+++ two_sum/two_sum_test.go
@@ -13,7 +13,7 @@ const (
 func TestTwoSumI(t *testing.T) {
 	tests := [][]int{
 		[]int{2, 7, 11, 15},
-		[]int{0, 9, 7, 3, 3, 4, 10},
+		[]int{0, 9, 7, 3, 4, 10},
 		[]int{1, 2, 8, 10},
 	}
 
@@ -25,12 +25,41 @@ func TestTwoSumI(t *testing.T) {
 
 	expectedResults := [][]int{
 		[]int{0, 1},
-		[]int{1, 6},
+		[]int{1, 5},
 		[]int{0, 0},
 	}
 
 	for i := range tests {
 		result := twoSumI(tests[i], targets[i])
+		if reflect.DeepEqual(expectedResults[i], result) == true {
+			t.Logf("\t%s\tCase: %v\tTarget: %v\tResult: %v", succeed, tests[i], targets[i], result)
+		} else {
+			t.Errorf("\t%s\tCase: %v\tTarget: %v\tResult: %v", failed, tests[i], targets[i], result)
+		}
+	}
+}
+
+func TestTwoSumII(t *testing.T) {
+	tests := [][]int{
+		[]int{2, 7, 11, 15},
+		[]int{0, 3, 4, 7, 9, 10},
+		[]int{1, 2, 8, 10},
+	}
+
+	targets := []int{
+		9,
+		19,
+		100,
+	}
+
+	expectedResults := [][]int{
+		[]int{0, 1},
+		[]int{4, 5},
+		[]int{0, 0},
+	}
+
+	for i := range tests {
+		result := twoSumII(tests[i], targets[i])
 		if reflect.DeepEqual(expectedResults[i], result) == true {
 			t.Logf("\t%s\tCase: %v\tTarget: %v\tResult: %v", succeed, tests[i], targets[i], result)
 		} else {
